e6e7aeed438aad7a5d5fdf8ce886ff5908b34f56
86130 <2635121825@qq.com>
2021-04-26 23:01:46 +0800

    mess

---
+++ src/main/java/com/example/oauthbaidu/MyAuthenticationFailureHandler.java
@@ -0,0 +1,25 @@
+package com.example.oauthbaidu;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(mapper.writeValueAsString(exception.getMessage()));
+    }
+}
---
+++ src/main/java/com/example/oauthbaidu/UserController.java
@@ -0,0 +1,15 @@
+package com.example.oauthbaidu;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("index")
+    public Object index(Authentication authentication){
+        return authentication;
+    }
+
+}
